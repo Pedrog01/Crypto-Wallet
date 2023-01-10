4756b2b001750234759aaf9a3443b804e9aa3d8b
Pedrog01 <pedro7109.j@gmail.com>
2023-01-10 10:40:24 -0300

    Identifying environments in Rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+       elsif Rails.env.profuction
+        "Produção"
+       else
+        "teste"
+       end
+    end
 end
