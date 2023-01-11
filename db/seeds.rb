e76a1af449d60f19bb3a5ec04c5a65eed8541979
Pedrog01 <pedro7109.j@gmail.com>
2023-01-11 10:47:31 -0300

    refactoring the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Caadastrando as moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...")
+spinner.auto_spin 
 
-Coin.create!(
-    description:"Bitcoin",
-    acronym:"BTC",
-    url_image: "https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"
-)
+coins =  
+    [
+        {description:"Bitcoin",
+        acronym:"BTC",
+        url_image: "https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"},
 
-Coin.create!(
-    description:"Ethereum",
-    acronym:"ETH",
-    url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"
-)
+        {description:"Ethereum",
+        acronym:"ETH",
+        url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"},
 
+        {description:"Dash",
+        acronym:"DASH",
+        url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"}
+    ]
+
+    coins.each do |coin| 
+        sleep(1)
+        Coin.find_or_create_by!(coin) 
+    end
+spinner.success("(Concluído!)")
+
+=begin
+    
+rescue => exception
+    
+ensure
+    
+end
 Coin.create!(
-    description:"Dash",
-    acronym:"DASH",
-    url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"
+    [
+        {description:"Bitcoin",
+        acronym:"BTC",
+        url_image: "https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"},
+
+        {description:"Ethereum",
+        acronym:"ETH",
+        url_image: "https://cryptologos.cc/logos/ethereum-eth-logo.png"},
+
+        {description:"Dash",
+        acronym:"DASH",
+        url_image: "https://cryptologos.cc/logos/dash-dash-logo.png"}
+    ]
 )
+=end
 
 puts "Moedas cadastradas com sucesso!..."
